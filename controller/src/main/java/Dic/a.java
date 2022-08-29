2b6e97a5c1803650849a8728c5b5aebcce2a5067
aiyksgd <2806624708@qq.com>
2022-08-29 11:11:27 +0800

    Initial commit

---
+++ controller/src/main/java/Dic/a.java
@@ -0,0 +1,10 @@
+package Dic;
+
+/**
+ * @author ：aiyk
+ * @date ：Created in 2022/8/29 8:56
+ * @description：1
+ * @modified By：
+ */
+public class a {
+}
---
+++ mapper/src/main/java/Dic/mapper/SsmpDicMapper.java
@@ -0,0 +1,23 @@
+package Dic.mapper;
+
+
+
+
+import Dic.entity.Ssmp;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author ayk
+ * @since 2021-12-02
+ */
+
+public interface SsmpDicMapper extends BaseMapper<Ssmp> {
+
+	
+
+}
